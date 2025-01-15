f071aaf8e7b17fada56b2a270911d2f6afd961c8
Stefan Junker <mail@stefanjunker.de>
2025-01-15 10:36:18 +0100

    remove checks/ for max consiseness

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,41 @@
 { pkgs, flake, ... }:
 
+let
+  testModule =
+    { pkgs }:
+
+    pkgs.testers.runNixOSTest (
+      { ... }:
+      {
+        name = "nixos-test";
+
+        nodes.machine =
+          { pkgs, ... }:
+          {
+            imports = [
+              (
+                { ... }:
+
+                {
+                  config =
+                    # mysteriously broken:
+                    pkgs.lib.mkIf true { };
+
+                  # works:
+                  # lib.mkIf true { };
+                }
+              )
+            ];
+          };
+
+        testScript = _: '''';
+      }
+    );
+
+in
+
 pkgs.runCommand "repro"
-  { passthru.tests.repro = import ../checks/nixos-test.nix { inherit pkgs flake; }; }
+  { passthru.tests.repro = testModule { inherit pkgs; }; }
   ''
     touch $out
   ''
