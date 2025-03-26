8d868e03af1beb4a2591b230c5a79504fd8ce32e
shirakami-chigusa <149283699+shirakami-chigusa@users.noreply.github.com>
2025-03-27 00:23:03 +0900

    set nixpkgs unstable like the other templates.

--- templates/home-manager-standalone/flake.nix
+++ templates/home-manager-standalone/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
     blueprint = {
       url = "github:numtide/blueprint";
@@ -8,7 +8,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
