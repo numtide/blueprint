f612011b803437aa7ede8ee010bd389b814cc983
Yu-Min Peng <leporuid@icloud.com>
2026-04-09 15:33:33 +0800

    Update templates/red-tape/flake.nix with red-tape inputs and mkFlake re-export (#10)

--- templates/red-tape/flake.nix
+++ templates/red-tape/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake using red-tape's mkFlake via blueprint";
+  description = "Red-tape template using blueprint's mkFlake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
@@ -8,10 +8,15 @@
       url = "github:numtide/blueprint";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    red-tape = {
+      url = "github:phaer/red-tape";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # Use the richer red-tape mkFlake, re-exported by blueprint.
-  # It is built on the adios-flake module system, giving you
-  # composable contrib modules for home-manager, nix-darwin, etc.
-  outputs = inputs: inputs.blueprint.mkFlake { inherit inputs; src = ./.; };
+  outputs = inputs: (inputs.blueprint { inherit inputs; src = ./.; }) // {
+    # Re-export red-tape's mkFlake for direct access
+    inherit (inputs.red-tape) mkFlake;
+  };
 }
